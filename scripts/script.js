a0239af2d13b5bd129b4d3a7f7a31d6327c076ad
Kanagarajkj <kanagarajkj98@gmail.com>
2022-04-18 11:18:58 +0530

    First  Commit

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+// const calcAverage = (scoreOne, scoreTwo, scoreThree) =>
+//    (scoreOne + scoreTwo + scoreThree) /3;
+
+// const avgDolphins = calcAverage();
+
+// const avgKoalas = calcAverage();
+
+// const checkWinner = function(avgDolphins, avgKoalas){
+
+// }
+
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
+
+// console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// const friends = ['rajesh', 'kumar', 'raj'];
+
+// console.log(friends[0]);
+// console.log(friends[1]);
+// console.log(friends[2]);
+
+// console.log(friends.length 3);
+// console.log(friends.length === 3);
+
